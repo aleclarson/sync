48b388147ee2d04ed3c8e9715dcde0caa5913eac
aleclarsoniv <alec.stanford.larson@gmail.com>
2016-09-14 13:10:25 -0700

    Update project structure

--- js/iterable.js
+++
@@ -1,9 +0,0 @@
-var PureObject, Typle;
-
-PureObject = require("PureObject");
-
-Typle = require("Typle");
-
-module.exports = Typle([Array, Object, PureObject]);
-
-//# sourceMappingURL=map/iterable.map
